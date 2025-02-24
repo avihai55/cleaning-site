f1c480a01b53d258f578e50cd804918eee9b470b
Avihai Kadoosi <50479623+avihai55@users.noreply.github.com>
2025-02-24 19:49:00 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,18 +1,1 @@
 
-function SendEmail() {
-  location.href = "mailto:simhakadoosi@gmail.com";
-}
-
-
-function filterProducts(category) {
-	
-            const products = document.querySelectorAll('.product');
-
-            products.forEach(product => {
-                if (category === 'all' || product.getAttribute('data-category') === category) {
-                    product.style.display = 'block';
-                } else {
-                    product.style.display = 'none';
-                }
-            });
-        }
